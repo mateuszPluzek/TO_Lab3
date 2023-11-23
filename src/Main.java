eacf6a9e89df9e92540e677bf989a2f83d35a4f6
mateusz.pluzek <mateusz.pluz@gmail.com>
2023-11-23 15:45:22 +0100

    Added the ability for the pop to move

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int stepsPerSecond = 24;
+        int sleepTime = 1000 / stepsPerSecond;
+
+        int iterator_flag = 0;
+
+//        TEST OBJECT
+        Pop p1 = new Pop(0,0,new Vector2D(3.0,5.0), 1);
+//        System.out.println(p1.getDirection());
+//        System.out.println(p1.getMovement());
+
+//       main loop
+        while(true) {
+            System.out.println("p1 coord [x:" + p1.getCord_x() + ", y:" + p1.getCord_y() + "]");
+            p1.move();
+
+            //Setting the static framerate
+            try {
+             Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //exit condition
+            iterator_flag++;
+            if(iterator_flag >= 10 * 24) {
+                break;
+            }
+        }
     }
 }
--- src/Pop.java
+++ src/Pop.java
@@ -7,9 +7,46 @@ public class Pop {
     private double cord_x;
     private double cord_y;
     private Vector2D direction;
+    private Vector2D movement;
+//    speed is represented in m/s
     private double speed;
 
+    public Pop(double window_h, double window_w) {
+//           TODO set random values
+    }
 
+    public Pop(double x, double y, Vector2D direction, double speed) {
+        this.cord_x = x;
+        this.cord_y = y;
+        this.speed = speed;
+        this.direction = direction;
+        this.calculateDirection(direction, speed);
+    }
+
+
+    public void calculateDirection(Vector2D direction, double speed) {
+        double ratioX, ratioY;
+        if(direction.getComponents()[0] > direction.getComponents()[1]) {
+           ratioX = direction.getComponents()[1] / direction.getComponents()[0];
+           ratioY = 1.0 - ratioX;
+        }
+        else if(direction.getComponents()[0] < direction.getComponents()[1]) {
+            ratioY = direction.getComponents()[0] / direction.getComponents()[1];
+            ratioX = 1.0 - ratioY;
+        }
+        else {
+            ratioX = 0.5;
+            ratioY = 0.5;
+        }
+
+        this.movement = new Vector2D(speed * ratioX,speed * ratioY);
+    }
+    public void move() {
+        this.cord_x += this.movement.getComponents()[0];
+        this.cord_y += this.movement.getComponents()[1];
+    }
+
+//Getters and setters
     public void setCord_x(double cord_x) {
         this.cord_x = cord_x;
     }
@@ -20,10 +57,12 @@ public class Pop {
 
     public void setDirect(Vector2D direct) {
         this.direction = direct;
+        calculateDirection(this.direction, this.speed);
     }
 
     public void setSpeed(double speed) {
         this.speed = speed;
+        calculateDirection(this.direction, this.speed);
     }
 
     public double getCord_x() {
@@ -38,7 +77,11 @@ public class Pop {
         return speed;
     }
 
-    public double[] getDirection() {
-        return direction.getComponents();
+    public Vector2D getDirection() {
+        return direction;
+    }
+
+    public Vector2D getMovement() {
+        return movement;
     }
 }
